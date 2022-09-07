8f2cb96f1615c118f8c119bdab1b7e8476a2d5be
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-09-07 09:58:26 +0200

    feat(http): add header and query validators (#31)

---
+++ src/lib/primitives/ext/query.ts
@@ -0,0 +1,14 @@
+import type { CstNode } from '../../cst/cst'
+import { isCstNode } from '../../cst/cst'
+import type { SchemaOptions } from '../base'
+import type { LazyObjectOptions, ObjectPropertiesArg } from '../object/object'
+import { $object } from '../object/object'
+import { $validator } from '../validator/validator'
+
+export function $query(properties: CstNode | ObjectPropertiesArg, options?: SchemaOptions<LazyObjectOptions>) {
+    const validator = $validator(
+        isCstNode(properties) ? properties : $object(properties, { additionalProperties: true, ...options })
+    )
+    validator.description.ajvOptions = { ...validator.description.ajvOptions, coerceTypes: true }
+    return validator
+}
