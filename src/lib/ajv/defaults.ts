9bdc3d5a93a2db42b4ff9aa385d4023184b64c25
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-09-07 10:05:08 +0200

    refactor: reduce compiled schema size (#32)
    * feat(http): add header and query validators
    
    * refactor: reduce compiled schema size

--- src/lib/ajv/defaults.ts
+++ src/lib/ajv/defaults.ts
@@ -1,9 +1,13 @@
-export const defaultAjvConfig = {
+import type { Options } from 'ajv'
+
+export const defaultAjvConfig: Options = {
     strict: true,
     strictTypes: true,
     useDefaults: true,
     removeAdditional: false,
     strictSchema: false,
+    loopRequired: 5,
+    loopEnum: 5,
     code: {
         optimize: true,
     },
