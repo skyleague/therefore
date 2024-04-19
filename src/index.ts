cf8ed1908a48400265f80161a951d628efdee181
pevisscher <2388946+pevisscher@users.noreply.github.com>
2024-04-19 16:11:43 +0200

    fix: use default import for bin name (#126)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import * as commands from './commands/index.js'
-
-import { bin } from '../package.json' assert { type: 'json' }
-
 import type { CommandModule } from 'yargs'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-
+import packageJSON from '../package.json' with { type: 'json' }
+import * as commands from './commands/index.js'
 export type { OpenapiV3 } from './types/openapi.type.js'
 export * from './lib/visitor/index.js'
 export type { InferSchemaType, Schema } from './lib/types.js'
 export * from './lib/cst/index.js'
 export * from './lib/primitives/index.js'
+
+const { bin } = packageJSON
 
 /**
  * @internal
