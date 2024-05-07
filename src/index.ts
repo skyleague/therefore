564275223ced690b30f9dcbecf2c40dfcc71041d
pevisscher <p.e.visscher@skyleague.io>
2024-05-07 14:04:53 +0000

    feat: expose parser type (#129)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import packageJSON from '../package.json' with { type: 'json' }
 import * as commands from './commands/index.js'
 export type { OpenapiV3 } from './types/openapi.type.js'
 export * from './lib/visitor/index.js'
-export type { InferSchemaType, Schema } from './lib/types.js'
+export type { InferSchemaType, Schema, Parser } from './lib/types.js'
 export * from './lib/cst/index.js'
 export * from './lib/primitives/index.js'
 
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,11 @@
-import type { AnySchema, ErrorObject } from 'ajv'
+import type { AnySchema, DefinedError, ErrorObject } from 'ajv'
 
 export type InferSchemaType<T> = T extends { is: (o: unknown) => o is infer S } ? S : never
 export interface Schema<T> {
     errors: ErrorObject[] | undefined
     schema: AnySchema
     is: (o: unknown) => o is T
+}
+export interface Parser<T> extends Schema<T> {
+    parse: (o: unknown) => { right: T } | { left: DefinedError[] }
 }
