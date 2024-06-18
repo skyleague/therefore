8abd89c92f89f9ee41e2b1098b28c7dcf8444e57
pevisscher <2388946+pevisscher@users.noreply.github.com>
2024-06-18 16:11:37 +0200

    fix: handle unknown type in ref instantiation (#151)

--- src/lib/primitives/ref/node.ts
+++ src/lib/primitives/ref/node.ts
@@ -1,3 +1,4 @@
+import type { IsUnknown } from '@skyleague/axioms/types'
 import type { Node } from '../../cst/node.js'
 import type { ArrayType } from '../array/array.js'
 import type { BooleanType } from '../boolean/boolean.js'
@@ -9,25 +10,27 @@ import type { StringType } from '../string/string.js'
 import type { UnknownType } from '../unknown/unknown.js'
 
 type AsObject<T> = T extends Record<string, Node> ? ObjectType<T> : UnknownType
-export type SchemaAsNode<S> = undefined extends S
-    ? OptionalType<SchemaAsNode<Exclude<S, undefined>>>
-    : null extends S
-      ? NullableType<SchemaAsNode<Exclude<S, null>>>
-      : S extends infer T
-        ? T extends undefined
-            ? never
-            : T extends string
-              ? StringType
-              : T extends number
-                ? NumberType
-                : T extends boolean
-                  ? BooleanType
-                  : T extends (infer U)[]
-                    ? ArrayType<SchemaAsNode<U>>
-                    : // biome-ignore lint/suspicious/noExplicitAny: needed for matching
-                      T extends Record<string, any>
-                      ? AsObject<{
-                            [K in keyof T]-?: SchemaAsNode<T[K]>
-                        }>
-                      : UnknownType
-        : never
+export type SchemaAsNode<S> = IsUnknown<S> extends true
+    ? UnknownType
+    : undefined extends S
+      ? OptionalType<SchemaAsNode<Exclude<S, undefined>>>
+      : null extends S
+        ? NullableType<SchemaAsNode<Exclude<S, null>>>
+        : S extends infer T
+          ? T extends undefined
+              ? never
+              : T extends string
+                ? StringType
+                : T extends number
+                  ? NumberType
+                  : T extends boolean
+                    ? BooleanType
+                    : T extends (infer U)[]
+                      ? ArrayType<SchemaAsNode<U>>
+                      : // biome-ignore lint/suspicious/noExplicitAny: needed for matching
+                        T extends Record<string, any>
+                        ? AsObject<{
+                              [K in keyof T]-?: SchemaAsNode<T[K]>
+                          }>
+                        : UnknownType
+          : never
