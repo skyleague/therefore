714a9f76522253e85e9f39374bf31be2814a2330
pevisscher <2388946+pevisscher@users.noreply.github.com>
2024-09-09 14:35:33 +0200

    feat: improve RestClientBuilder type handling and add DefaultTestClient (#164)

--- examples/edges/client.client.ts
+++ examples/edges/client.client.ts
@@ -10,6 +10,82 @@ import type { DefinedError } from 'ajv'
 import { got } from 'got'
 import type { CancelableRequest, Got, Options, OptionsInit, Response } from 'got'
 
+import { GetEmployeesResponse200, GetEmployeesResponseDefault } from './client.type.js'
+
+/**
+ * openapi
+ */
+export class DefaultTestClient {
+    public client: Got
+
+    public constructor({
+        prefixUrl,
+        options,
+    }: {
+        prefixUrl: string
+        options?: Options | OptionsInit
+    }) {
+        this.client = got.extend(...[{ prefixUrl, throwHttpErrors: false }, options].filter((o): o is Options => o !== undefined))
+    }
+
+    public getEmployees(): Promise<
+        | SuccessResponse<'201' | '202' | '203' | '204' | '205' | '206' | '207' | '208' | '226', GetEmployeesResponseDefault>
+        | SuccessResponse<'200', GetEmployeesResponse200>
+        | FailureResponse<StatusCode<2>, string, 'response:body', IncomingHttpHeaders>
+        | FailureResponse<StatusCode<1 | 3 | 4 | 5>, string, 'response:statuscode', IncomingHttpHeaders>
+    > {
+        return this.awaitResponse(
+            this.client.get('employees', {
+                responseType: 'json',
+            }),
+            {
+                200: GetEmployeesResponse200,
+                default: GetEmployeesResponseDefault,
+            },
+        ) as ReturnType<this['getEmployees']>
+    }
+
+    public async awaitResponse<
+        I,
+        S extends Record<PropertyKey, { parse: (o: I) => { left: DefinedError[] } | { right: unknown } } | undefined>,
+    >(response: CancelableRequest<Response<I>>, schemas: S) {
+        const result = await response
+        const status =
+            result.statusCode < 200
+                ? 'informational'
+                : result.statusCode < 300
+                  ? 'success'
+                  : result.statusCode < 400
+                    ? 'redirection'
+                    : result.statusCode < 500
+                      ? 'client-error'
+                      : 'server-error'
+        const validator = schemas[result.statusCode] ?? schemas.default
+        const body = validator?.parse?.(result.body)
+        if (result.statusCode < 200 || result.statusCode >= 300) {
+            return {
+                statusCode: result.statusCode.toString(),
+                status,
+                headers: result.headers,
+                left: body !== undefined && 'right' in body ? body.right : result.body,
+                validationErrors: body !== undefined && 'left' in body ? body.left : undefined,
+                where: 'response:statuscode',
+            }
+        }
+        if (body === undefined || 'left' in body) {
+            return {
+                statusCode: result.statusCode.toString(),
+                status,
+                headers: result.headers,
+                left: result.body,
+                validationErrors: body?.left,
+                where: 'response:body',
+            }
+        }
+        return { statusCode: result.statusCode.toString(), status, headers: result.headers, right: result.body }
+    }
+}
+
 /**
  * openapi
  */
