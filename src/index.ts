f680c557ebdf53665f652878bea624b959c52d2a
pevisscher <2388946+pevisscher@users.noreply.github.com>
2025-07-23 14:01:23 +0200

    fix: reorder exports to prevent circular dependencies (#246)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+// biome-ignore assist/source/organizeImports: this needs to come first to prevent circular dependencies
+export * from './lib/visitor/index.js'
+
 export * from './lib/cst/index.js'
 export * from './lib/primitives/index.js'
 export type { InferSchemaType, Parser, Schema } from './lib/types.js'
-export * from './lib/visitor/index.js'
 export type { OpenapiV3 } from './types/openapi.type.js'
